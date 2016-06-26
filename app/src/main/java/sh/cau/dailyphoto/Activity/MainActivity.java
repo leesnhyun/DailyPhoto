48cc9cd83b9dd420def2c59125cc7e4da9a9d31b
lee senghyun <leesnhyun@naver.com>
2016-06-27 00:40:50 +0900

    bug modified

--- app/src/main/java/sh/cau/dailyphoto/Activity/MainActivity.java
+++ app/src/main/java/sh/cau/dailyphoto/Activity/MainActivity.java
@@ -2,14 +2,27 @@ package sh.cau.dailyphoto.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import sh.cau.dailyphoto.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize
+        initRefToolbar();
+
     }
+
+    private void initRefToolbar(){
+        this.toolbar = (Toolbar)findViewById(R.id.toolbar_top_menu);
+        this.setSupportActionBar(toolbar);
+    }
+
 }
